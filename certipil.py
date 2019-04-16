e8a00ac7b93c84e7aee34a61356e34be99f22e51
Kousik Rajesh <kousik18@iitg.ac.in>
2019-04-17 01:08:15 +0530

    Update certipil.py

--- certipil.py
+++ certipil.py
@@ -1,11 +1,18 @@
 from PIL import Image, ImageFont, ImageDraw
+
+
+
 name="Kousik Rajesh"
 city="Greater Noida"
+i=3#index corresponding to certificate
+rank="123"
+squad="Junior"
+
 
 names=["participation,jpg","gold.jpg","silver.jpg","citytopper.jpg"]
-values=[[123,889,832,175,470,1045],[120,888,738,216,477,830],[120,888,730,216,477,822],[120,888,832,492,744,920]]
+values=[[123,889,832,175,470,1045],[120,888,738,216,477,830,186,468,1148,613,852,1084],[120,888,730,216,477,822,228,508,1138,655,897,1075],[120,888,832,492,744,920,123,324,982]]
 
-i=1#index corresponding to certificate
+
 certi=Image.open(names[i])
 
 nameblankStartX=values[i][0]
@@ -16,10 +23,28 @@ cityblankStartX=values[i][3]
 cityblankEndX=values[i][4]
 cityblankY=values[i][5]
 
+if i==0:
+    pass
+elif i==3:
+    squadStartX=values[i][6]
+    squadEndX=values[i][7]
+    squadY=values[i][8]
 
+else:
+    squadStartX=values[i][6]
+    squadEndX=values[i][7]
+    squadY=values[i][8]
+    rankStartX=values[i][9]
+    rankEndX=values[i][10]
+    rankY=values[i][11]
+
+    
 nameFontSize=50#default size for small names
 cityFontSize=35
+rankFontSize=35
+squadFontSize=35
 draw = ImageDraw.Draw(certi)
+
 while True:
     
     font = ImageFont.truetype("Montserrat-Regular.ttf", nameFontSize) 
@@ -39,12 +64,31 @@ while True:
     else:
         break
 citySize=font.getsize(city)
-
-
-
-
-
-
 draw.text(((cityblankStartX+cityblankEndX)/2-citySize[0]/2, cityblankY-citySize[1]), city, font=font,fill=(0,0,0))
+
+if(i!=0):
+    while True:
+    
+        font = ImageFont.truetype("Montserrat-Regular.ttf", squadFontSize) 
+        
+        if(font.getsize(squad)[0]>(squadEndX-squadStartX)):
+            squadFontsize-=1
+        else:
+            break
+    squadSize=font.getsize(squad)
+    draw.text(((squadStartX+squadEndX)/2-squadSize[0]/2, squadY-squadSize[1]), squad, font=font,fill=(0,0,0))
+        
+if(i!=0 and i!=3):
+    while True:
+    
+        font = ImageFont.truetype("Montserrat-Regular.ttf", rankFontSize) 
+        
+        if(font.getsize(rank)[0]>(rankEndX-rankStartX)):
+            rankFontsize-=1
+        else:
+            break
+    rankSize=font.getsize(rank)
+    draw.text(((rankStartX+rankEndX)/2-rankSize[0]/2, rankY-rankSize[1]), rank, font=font,fill=(0,0,0))
+    
 certi.show()
 certi.save("certinamed.jpg")
